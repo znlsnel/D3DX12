22b9c06d2fa43cafda457bc975b9ae558c3bce94
Udi <dnel0605@gmail.com>
2022-12-15 19:08:11 +0900

    CreateDevice II

--- D3DX12_begin/main.cpp
+++ D3DX12_begin/main.cpp
@@ -2,14 +2,41 @@
 using namespace Microsoft::WRL;
 
 
-HRESULT __stdcall D3D12CreateDevice(IUnknown* pAdapter, D3D_FEATURE_LEVEL MinimumFeatureLevel, REFIID riid, void** ppDevice)
-{
-	return E_NOTIMPL;
-}
 
-int maint()
+int main() 
 {
-	ComPtr<ID3D12Debug> debugController;
-	ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)));
 
+	#if defined(DEBUG) || defined(_DEBUG)
+	{ // 디버그층 활성화
+		ComPtr<ID3D12Debug> debugController;
+		ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)));
+		debugController->EnableDebugLayer();
+	}
+	#endif
+
+
+	ComPtr<IDXGIFactory4> mdxgiFactory;
+	ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&mdxgiFactory)));
+
+
+	// 하드웨어 어댑터를 나타내는 장치를 생성
+	ComPtr<ID3D12Device> md3dDevice;// = ComPtr<ID3D12Device>();
+	HRESULT hardwareResult = D3D12CreateDevice(
+		nullptr,
+		D3D_FEATURE_LEVEL_11_0,
+		IID_PPV_ARGS(&md3dDevice));
+
+
+	// 실패했다면 WARP 어댑터를 나타내는 장치를 생성
+	if (FAILED(hardwareResult))
+	{
+		ComPtr<IDXGIAdapter> pWarpAdapter;// = ComPtr<IDXGIAdapter>();
+		ThrowIfFailed(mdxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&pWarpAdapter)));
+
+		ThrowIfFailed(D3D12CreateDevice(
+			pWarpAdapter.Get(),
+			D3D_FEATURE_LEVEL_11_0,
+			IID_PPV_ARGS(&md3dDevice)
+		));
+	}
 }
--- D3DX12_begin/main.h
+++ D3DX12_begin/main.h
@@ -1,13 +1,24 @@
 #pragma once
 
+#pragma comment(lib, "d3d12")
+#pragma comment(lib, "dxgi")
+
 #include <windows.h>
 #include <wrl.h>
 #include <d3dcommon.h>
+#include <exception>
 #include <d3d12sdklayers.h>
+#include <dxgi1_4.h>
 
-HRESULT WINAPI D3D12CreateDevice(
-	IUnknown* pAdapter,
-	D3D_FEATURE_LEVEL MinimumFeatureLevel,
-	REFIID riid,
-	void** ppDevice
-);
+//#include <dxgi.h>
+//#include <d3d12.h>
+//#include <ppltasks.h>	// create_task의 경우
+//#include <dxgidebug.h>
+
+
+inline void ThrowIfFailed(HRESULT hr)
+{
+	// DirectX API 오류를 탐지하기 위해 이 줄에 중단점 설정
+	if (FAILED(hr))
+		throw std::exception();
+}
