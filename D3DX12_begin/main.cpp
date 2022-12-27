6db489767b94cbb8a75e00118b67d13402acf505
Udi <dnel0605@gmail.com>
2022-12-27 20:55:01 +0900

    Win API
    설정의 문제인지 해당 파일에서는 실행이 안됨..

--- D3DX12_begin/main.cpp
+++ D3DX12_begin/main.cpp
@@ -1,386 +1,133 @@
 #include "main.h"
-using namespace Microsoft::WRL;
 
-// Rtv : Render Target view
-// Dsv : depth , stencil view 
-// Cbv : Constant buffer view 
-// Srv : Shader resource view
-// Uav : Unordered access view (순서 없는 접근 )
-int mRtvDescriptorSize;
-int mDsvDescriptorSize;
-int mCbvSrvDescriptorSize;
-
-// stencill buffer란 ?
-// 특정 픽셀들이 후면버퍼에 기록되지 않도록 하는 버퍼
-// ex) 그림자, 거울 랜더링시 사용됨
-
-#pragma region  COM
-
-ComPtr<ID3D12Device> md3dDevice;// = ComPtr<ID3D12Device>();
-ComPtr<IDXGIFactory4> mdxgiFactory;
-ComPtr<ID3D12Fence1> mFence;
-ComPtr<IDXGISwapChain> mSwapChain;
-ComPtr<ID3D12CommandQueue> mCommandQueue; // 명령 대기열
-ComPtr<ID3D12CommandAllocator> mDirectCmdListAlloc; // 명령 할당자
-ComPtr<ID3D12GraphicsCommandList> mCommandList; // 명령 목록
-ComPtr<ID3D12DescriptorHeap> mRtvHeap; // render target view Heap
-ComPtr<ID3D12DescriptorHeap> mDsvHeap; // Deap Stencil View Heap
-ComPtr<ID3D12Resource> mSwapChainBuffer[SwapChainBufferCount];
-ComPtr<ID3D12Resource> mDepthStencilBuffer;
-
-#pragma endregion
-
-
-
-D3D12_CPU_DESCRIPTOR_HANDLE DepthStencilView()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nShowCmd)
 {
-	return mDsvHeap->GetCPUDescriptorHandleForHeapStart();
+	// 우선 hInstance와 nShowCmd로 초기화 함수 (InitWindowsApp)를
+	// 호출해서 응용 프로그램 주 창을 초기화한다.
+	if (!InitWindowsApp(hInstance, nShowCmd))
+		return 0;
+	// 응용프로그램이 성공적으로 생성, 초기화되었다면 메시지 루프로
+	// 진입한다. 그 루프는 응용 프로그램이 종료되어야 함을 뜻하는 
+	// WM_QUIT 메시지를 받을 때까지 계속 돌아간다.
+	return Run();
 }
 
-D3D12_VIEWPORT vp;
-D3D12_RECT mScissorRect;
-
-DXGI_FORMAT mBackBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
-DXGI_FORMAT mDepthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
-
-CD3DX12_HEAP_PROPERTIES properties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
-CD3DX12_RESOURCE_BARRIER resourceBarrier = CD3DX12_RESOURCE_BARRIER::Transition(
-	mDepthStencilBuffer.Get(),
-	D3D12_RESOURCE_STATE_COMMON,
-	D3D12_RESOURCE_STATE_DEPTH_WRITE);
-
-
-
-
-
-int main() 
+bool InitWindowsApp(HINSTANCE instanceHandle, int show)
 {
+	// 창을 생성할 때 가장 먼저 할일 일은 창의 몇몇 특성을 서술하는
+	// WNDCLASS 구조체를 채우는 것이다.
+	WNDCLASS wc;
 
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = instanceHandle;
+	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(0, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = 0;
+	wc.lpszClassName = L"BasicWndClass";
 
-#pragma region 장치 생성
-
-/*
-
-#if defined(DEBUG) || defined(_DEBUG)
-	{ // 디버그층 활성화
-		ComPtr<ID3D12Debug> debugController;
-		ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)));
-		debugController->EnableDebugLayer();
-	}
-#endif
-
-
-	ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&mdxgiFactory)));
-
-	// 하드웨어 어댑터를 나타내는 장치를 생성
-	HRESULT hardwareResult = D3D12CreateDevice(
-		nullptr,
-		D3D_FEATURE_LEVEL_11_0,
-		IID_PPV_ARGS(&md3dDevice));
-
-
-	// 실패했다면 WARP 어댑터를 나타내는 장치를 생성
-	if (FAILED(hardwareResult))
+	// 다음으로, 이 WNDCLASS 인스턴스 ('창 클래스')를 Windows에 등록한다.
+	// 그래야 다음 단계에서 이 창 클래스에 기초해서 창을 생성할 수 있다.
+	if (!RegisterClass(&wc))
 	{
-		ComPtr<IDXGIAdapter> pWarpAdapter;// = ComPtr<IDXGIAdapter>();
-		ThrowIfFailed(mdxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&pWarpAdapter)));
-
-		ThrowIfFailed(D3D12CreateDevice(
-			pWarpAdapter.Get(),
-			D3D_FEATURE_LEVEL_11_0,
-			IID_PPV_ARGS(&md3dDevice)
-		));
+		MessageBox(0, L"RegisterClass FAILED", 0, 0);
+		return false;
 	}
-*/
 
-#pragma endregion
+	// WNDCLASS 인스턴스가 성공적으로 등록되었다면 CreateWindow 함수로
+	// 창을 생성할 수 있다. 이 함수는 성공 시에는 생성된 창의 핸들(HWND 형식의 값)을,
+	// 실패 시에는 값이 0인 핸들을 돌려준다. 창 핸들은 특정 창을 지칭하는 데 쓰이는 값으로,
+	// Windows가 내부적으로 관리한다. 창을 다루는 Win32 API 함수 중에는 자신이 작업할 창을 식별하기
+	// 위해 이 HWND 값을 받는 것들이 많다.
 
-#pragma region  DescriptorSize
-	/*
-	ThrowIfFailed(md3dDevice->CreateFence(
-		0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&mFence)));
-
-	mRtvDescriptorSize = md3dDevice->GetDescriptorHandleIncrementSize(
-		D3D12_DESCRIPTOR_HEAP_TYPE_RTV
+	ghMainWnd = CreateWindow(
+		L"BasicWndClass",			// 사용할 창 클래스의 이름
+		L"Win32Basic",				// 창의 제목
+		WS_OVERLAPPEDWINDOW,	// 스타일 플래그들
+		CW_USEDEFAULT,			// 창 위치의 x 좌표성분
+		CW_USEDEFAULT,			// 창 위치의 y 좌표성분
+		CW_USEDEFAULT,			// 창의 너비
+		CW_USEDEFAULT,			// 창의 높이
+		0,							// 부모 창 핸들
+		0,							// 메뉴 핸들
+		instanceHandle,				// 응용 프로그램 인스턴스 핸들
+		0							// 추가 생성 플래그들
 	);
-	mDsvDescriptorSize = md3dDevice->GetDescriptorHandleIncrementSize(
-		D3D12_DESCRIPTOR_HEAP_TYPE_DSV
-	);
-	mCbvSrvDescriptorSize = md3dDevice->GetDescriptorHandleIncrementSize(
-		D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV
-		);
-	
-	*/
-#pragma endregion
-	   
-#pragma region 4X MSAA 품질 수준 지원 점검
-	/*
-	D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS msQualityLevels;
-	msQualityLevels.Format = mBackBufferFormat;
-	msQualityLevels.SampleCount = 4;
-	msQualityLevels.Flags = D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE;
-	msQualityLevels.NumQualityLevels = 0;
-
-	ThrowIfFailed(md3dDevice->CheckFeatureSupport(
-		D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS,
-		&msQualityLevels,
-		sizeof(msQualityLevels)));
-	m4xMsaaQuality = msQualityLevels.NumQualityLevels;
-	assert(m4xMsaaQuality > 0 && "Unexpected MSAA quality level.");
-	*/
-	
-#pragma endregion
-
-#pragma region 명령 대기열과 명령 목록 생성
-
-	/*
-	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
-	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
-	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-
-	// CommandQueue 명령 대기열
-	ThrowIfFailed(md3dDevice->CreateCommandQueue(
-		&queueDesc, IID_PPV_ARGS(&mCommandQueue)));
-
-	// CommandAllocator 명령 할당자
-	ThrowIfFailed(md3dDevice->CreateCommandAllocator(
-		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		IID_PPV_ARGS(mDirectCmdListAlloc.GetAddressOf())));
-
-	// CommandList 명령 목록
-	ThrowIfFailed(md3dDevice->CreateCommandList(
-		0,
-		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		mDirectCmdListAlloc.Get(),
-		nullptr, // 그리기 명령 제출할게 없으므로 nullptr
-		IID_PPV_ARGS(mCommandList.GetAddressOf())));
-
-	// 명령 목록을 처음 호출할 때 Resetd을 호출하는데
-	// Reset을 호출하려면 명령 목록이 닫혀있어야 함
-	mCommandList->Close();
-	*/
-	
-
-#pragma endregion
-
-#pragma region  교환 사슬의 서술과 생성
-
-/*
-	mSwapChain.Reset();
-
-	DXGI_SWAP_CHAIN_DESC sd;
-	sd.BufferDesc.Width = mClientWidth;
-	sd.BufferDesc.Height = mClientHeight;
-	sd.BufferDesc.RefreshRate.Denominator = 1; // 1분에
-	sd.BufferDesc.RefreshRate.Numerator = 60; // 60번
-	sd.BufferDesc.Format = mBackBufferFormat;
-	sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED; // 픽셀 순서 상관없이 출력
-	sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED; // 스케일링 지정X (확장 배율, 디스플레이 중앙에 위치하게 하는등의 방법도 있음)
-	sd.SampleDesc.Count = m4xMsaaState ? 4 : 1;
-	sd.SampleDesc.Quality = m4xMsaaState ? (m4xMsaaQuality - 1) : 0;
-	sd.Windowed = true; // 창모드
-	sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-	//이 플래그를 사용하여 비트 블록 전송 모델을 지정IDXGISwapChain1::P resent1을 호출 후 DXGI가 백 버퍼의 콘텐츠를 삭제하도록 지정
-	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
-	// 창모드에서 정체 화면 모드로 전환하면 응용 프로그램의 창 크기랑 일치하게 디스플레이 모드가 변경됨
-
-	ThrowIfFailed(mdxgiFactory->CreateSwapChain(
-		mCommandQueue.Get(), // 명령 대기열의 포인터를 반환
-		&sd,
-		mSwapChain.GetAddressOf() // 포인터의 주소값을 반환
-	));
-*/
-
-	
-#pragma endregion
-
-#pragma region 서술자 힙 생성
-/*
-	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc;
-	rtvHeapDesc.NumDescriptors = SwapChainBufferCount; //  Heap desc 수
-	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
-	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-	rtvHeapDesc.NodeMask = 0;
-
-	ThrowIfFailed(md3dDevice->CreateDescriptorHeap(
-		&rtvHeapDesc, IID_PPV_ARGS(mRtvHeap.GetAddressOf())
-	));
-
-	D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc;
-	rtvHeapDesc.NumDescriptors = 1; //  Heap desc 수
-	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
-	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-	rtvHeapDesc.NodeMask = 0;
-
-	ThrowIfFailed(md3dDevice->CreateDescriptorHeap(
-		&dsvHeapDesc, IID_PPV_ARGS(mDsvHeap.GetAddressOf())
-	));
-*/
-	
-#pragma endregion
-
-#pragma region 렌더 대상 뷰(RTV) 생성
-	
-/*
-	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHeapHandle(
-		mRtvHeap->GetCPUDescriptorHandleForHeapStart());
-	
-	for (UINT i = 0; i < SwapChainBufferCount; i++)
+	if (ghMainWnd == 0)
 	{
-		// 교환 사실의 i번째 버퍼를 얻는다.!
-		ThrowIfFailed(mSwapChain->GetBuffer(
-			i, IID_PPV_ARGS(&mSwapChainBuffer[i]) ));
-
-		// 그 버퍼에 대한 RTV를 생성한다.!
-		md3dDevice->CreateRenderTargetView(
-			mSwapChainBuffer[i].Get(), nullptr, rtvHeapHandle);
-
-		// 힙의 다음 항목으로 넘어간다.
-		rtvHeapHandle.Offset(1, mRtvDescriptorSize);
+		MessageBox(0, L"CreateWiondow FAILED", 0, 0);
+		return false;
 	}
-*/
-#pragma endregion
 
-#pragma region 깊이, 스텐실 버퍼와 뷰 생성
-/*
-	D3D12_RESOURCE_DESC depthStencilDesc;
-	depthStencilDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
-	depthStencilDesc.Alignment = 0; 
-	depthStencilDesc.Width = mClientWidth;
-	depthStencilDesc.Height = mClientHeight;
-	depthStencilDesc.DepthOrArraySize = 1;
-	depthStencilDesc.MipLevels = 1;
-	depthStencilDesc.Format = mDepthStencilFormat;
-	depthStencilDesc.SampleDesc.Count = m4xMsaaState ? 4 : 1;
-	depthStencilDesc.SampleDesc.Quality = m4xMsaaState ? (m4xMsaaQuality - 1) : 0;
-	depthStencilDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
-	depthStencilDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+	// 창이 생성되어도 바로 화면에 나타나지는 않는다.
+	// 실제로 화면에 표시하고 갱신하기 위해서는 다음 두 함수를 호출해 주어야 한다.
+	// 두 함수 모두 갱신할 창의 핸들을 받는다는 점에 주목하기 바란다.
+	// 그 핸들은 함수가 표시 또는 갱신할 창이 무엇인지 알려주는 역할을 한다.
+	ShowWindow(ghMainWnd, show);
+	UpdateWindow(ghMainWnd);
 
-	D3D12_CLEAR_VALUE optClear;
-	optClear.Format = mDepthStencilFormat;
-	optClear.DepthStencil.Depth = 1.0f;
-	optClear.DepthStencil.Stencil = 0;
-	
+	return true;
+}
 
-	ThrowIfFailed(md3dDevice->CreateCommittedResource(
-		&properties, // 기본 힙 GPU가 접근할 자원이 담김
-		D3D12_HEAP_FLAG_NONE,
-		&depthStencilDesc,
-		D3D12_RESOURCE_STATE_COMMON,
-		&optClear,
-		IID_PPV_ARGS(mDepthStencilBuffer.GetAddressOf())
-	));
+int Run()
+{
+	MSG msg = { 0 };
 
-	// 전체 자원이 밉맵 수준 0에 대한 서술자를
-	// 해당 자원의 픽셀 형식을 적용해서 생성한다.
-	md3dDevice->CreateDepthStencilView(
-		mDepthStencilBuffer.Get(),
-		nullptr,
-		DepthStencilView()
-	);
-	// 자원을 초기 상태에서 깊이 버퍼로 사용할 수 있는 상태로 전이한다.
-	mCommandList->ResourceBarrier(
-		1,
-		&resourceBarrier
-	);
-*/
-
-
-
-
-
-
-#pragma endregion
-
-#pragma region 뷰포트 설정
-	/*
-	vp.TopLeftX = 0.0f;
-	vp.TopLeftY = 0.0f;
-	vp.Width = static_cast<float>(mClientWidth);
-	vp.Height = static_cast<float>(mClientHeight);
-	vp.MinDepth = 0.0f;
-	vp.MaxDepth = 1.0f;
-
-	mCommandList->RSSetViewports(1, &vp);
-	*/
-#pragma endregion
-
-#pragma region  가위 직사각형 설정
-	/*
-	mScissorRect = { 0, 0, mClientWidth / 2, mClientHeight / 2 };
-	mCommandList->RSSetScissorRects(1, &mScissorRect);
-	*/
-#pragma endregion
-
-#pragma region 성능 타이머
-	/*
-	// 정확한 시간 측정을 위해 사용
-
+	// WM_QUIT 메시지를 받을 때까지 루프를 돌린다. GetMessage() 함수는
+	// WM_QUIT 메시지를 받은 경우에만 0을 돌려주며, 그러면 결과적으로
+	// 루프가 종료된다. 만일 메시지 수신에서 오류가 있었으면 이 함수는
+	// -1을 돌려준다. 또한, GetMessage()를 호출하면 메시지가 도달할 때까지
+	// 응용 프로그램 스레드가 수면(sleep) 상태가 된다는 점도 주의하기 바란다.
+	BOOL bRet = 1;
+	while ((bRet = GetMessage(&msg, 0, 0, 0)) != 0)
 	{
-		__int64 currTime;
-		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
-
-		__int64 countsPerSec;
-		QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
-
-		double mSecondsPerCount = 1.0 / (double)countsPerSec;
+		// 오류
+		if (bRet == -1)
+		{
+			MessageBox(0, L"GetMessage FAILED", L"Error", MB_OK);
+			break;
+		}
+		else
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
 	}
-
-	// currTime 을 A라고 하고 그 다음에 QueryPerformanceCounter함수로 얻게되는 값을 B라고 할때
-	// 어떤 작업에 걸린 시간은 (B - A) * mSecondsPerCount 초이다
-	*/
-
-#pragma endregion
-
-#pragma region GameTimer 클래스
-	// main.h에 기술함
-#pragma endregion
-
-#pragma region	프레임 간 경과 시간
-	// 시간이 얼마나 흘렀는지 알아야함!
-	// x가 n번째 프레임을 랜더링할때 측정한 성능 타이머 값이고
-	// y가 그 이전 프레임에서의 성능 타이머 값이라고 하면
-	// x  - y가 두 프레임 사이의 경과 시간이라고 할 수 있음
-	
-	// main.h의 GameTimer클래스에 기술 TICK 함수
-	
-	// Tick함수 호출
-	// ----- main.h의 Run함수
-	// ----- GameTimer의 Reset함수
-#pragma endregion
-
-#pragma region 전체 시간
-	// 응용 프로그램이 시작된 이후에 흐른 시간을 뜻함 (정지된 시간 제외)
-	// 전체 시간을 구현하기 위해 GameTime 클래스는 다음과 같은 멤버 변수를 사용하
-	// mBaseTime, mPausedTime, mStopTime
-
-	// GameTimer 클래스에 설명
-
-	// GameTimer의 인스턴스를 추갈 생성하여 범용 '스톱워치'로 사용하는 것도 얼만든지 가능함
-	// 예를 들어 폭탄의 도화선에 불이 붙었을 때 새 GameTimer인스턴스를 생성하고 TotalTime이 5초가 넘으면 폭탄이 폭발하는 시건을 촉발하는 등으로 활용할 수 있을 것이다.
-#pragma endregion
-
-#pragma region d3dApp
-	/*
-	HINSTANCE mhAppInst = nullptr; // 응용 프로그램 핸들
-	HWND      mhMainWnd = nullptr; // 주 창 핸들
-	bool      mAppPaused = false;  // 일시 정지된 상태인가?
-	bool      mMinimized = false;  // 최소화된 상태인가?
-	bool      mMaximized = false;  // 최대화된 상태인가?
-	bool      mResizing = false;   // 크기 조정용 테두리를 끌고 있는 상태인가?
-	bool      mFullscreenState = false;// 전체화면 활성화 여부
-
-	// Set true to use 4X MSAA (?.1.8).  The default is false.
-	bool      m4xMsaaState = false;    // 4X MSAA enabled
-	UINT      m4xMsaaQuality = 0;      // quality level of 4X MSAA 
-	*/
-#pragma endregion
-
-
-#pragma region DX_WIN API
-
-#pragma endregion
-
-
+	return (int)msg.wParam;
 
 }
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM IParam)
+{
+	// 몇몇 메시지를 명시적으로 처리한다. 
+	// 처리한 메시지에 대해서는 반드시 0을 반환해야 함을 주의할 것.
+	switch (msg)
+	{
+		// 왼쪽 마우스 버튼이 눌렸으면 메시지 상자를 표시한다.
+	case WM_LBUTTONDOWN:
+		MessageBox(0, L"Hello, World", L"Hello", MB_OK);
+		return 0;
+
+		// Esc 키가 눌렸으면 주 응용프로그램 창을 파괴한다.
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			DestroyWindow(ghMainWnd);
+		return 0;
+	
+		// 파괴 메시지의 경우에는 종료 메시지를 보낸다
+		// 그러면 결과적으로 메시지 루프가 종료된다.
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	}
+	// 여기서 명시적으로 처리하지 않은 다른 메시지들은 기본 창
+	// 프로시저(DefWindowProc)에게 넘겨 준다. 이 창 프로시저가
+	// 반드시 DefWindowProc의 반환값을 돌려주어야 함을 주의하기 바란다.
+	return DefWindowProc(hWnd, msg, wParam, IParam);
+
+}
+
--- D3DX12_begin/main.h
+++ D3DX12_begin/main.h
@@ -1,288 +1,38 @@
 #pragma once
 
-//#pragma comment(lib, "d3d12")
-//#pragma comment(lib, "dxgi")
-
-#pragma comment(lib,"d3dcompiler.lib")
-#pragma comment(lib, "D3D12.lib")
-#pragma comment(lib, "dxgi.lib")
-
-
 #include <windows.h>
-#include <wrl.h>
-#include <d3dcommon.h>
-#include <exception>
-#include <d3d12sdklayers.h>
-#include <dxgi1_4.h>
-#include <assert.h>
-#include <comdef.h>
-#include <winnt.h>
-#include <math.h>
 
-#include "directx/d3dx12.h"
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+#include <SDKDDKVer.h>
 
-//#include <dxgi.h>
-//#include <ppltasks.h>	// create_task의 경우
-//#include <dxgidebug.h>
-
-#pragma region 장치 초기화 관련
-
-class GameTimer
-{
-public:
-	GameTimer()
-		: mSecondsPerCount(0.0), mDeltaTime(-1.0), mBaseTime(0), mStopTime(0),
-		mPausedTime(0), mPrevTime(0), mCurrTime(0), mStopped(false)
-	{
-		__int64 countsPerSec;
-		QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
-
-		mSecondsPerCount = 1.0 / (double)countsPerSec;
-	}
-
-	float TotalTime()const 
-	{
-		// 전체 시간을 반환
-		if (mStopped)
-		{
-			return (float)(((mStopTime - mPausedTime) - mBaseTime) * mSecondsPerCount);
-		}
-		else
-		{
-			return (float)(((mCurrTime - mPausedTime) - mBaseTime) * mSecondsPerCount);
-		}
-	} // 초 단위
-
-	float DeltaTime()const
-	{
-		return (float)mDeltaTime;
-	} // 초 단위
-
-	void Reset()
-	{
-		__int64 currTime;
-		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
-
-		// 첫프레임에서 이전 프레임이라는 것이 없으므로 현재 시간을 넣어줌
-		mPrevTime = currTime; 
-		mBaseTime = currTime;
-		mStopTime = 0;
-		mStopped = false;
-	} // 메시지 루프 이전에 호출
-
-	void Start()
-	{
-		__int64 startTime;
-		QueryPerformanceCounter((LARGE_INTEGER*)&startTime);
-
-		if (mStopped)
-		{
-			// 일시정지된 시간의 길이 = 재개된 시점의 시간 - 정지된 시점의 시간
-			mPausedTime += (startTime - mStopTime);
-
-			mPrevTime = startTime; // Start이후 Tick에 쓰일 mPrevTime에 시작시간을 넣어줌
-			mStopTime = 0;
-			mStopped = false;
-		}
-	} // 타이머를 시작 또는 재개할 때 호출
-
-	void Stop()
-	{
-		// 이미 정지 상태면 아무거또 안함
-		if (!mStopped)
-		{
-			__int64 currTime;
-			QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
-
-			// 현재 시간을 정지 시점 시간으로 지정
-			mStopTime = currTime; 
-			mStopped = true;
-		}
-	} // 타이머를 정지할 때 호출
-
-	void Tick()
-	{
-		if (mStopped)
-		{
-			mDeltaTime = 0.0;
-			return;
-		}
-
-		// 이번 프레임의 시간을 얻음
-		__int64 currTime;
-		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
-		mCurrTime = currTime;
-
-		// 작업 소요 시간 = (작업 수행 이후 현재 시간 - 작업 수행 전 시간) * mSecondsPerCount
-		mDeltaTime = (mCurrTime - mPrevTime) * mSecondsPerCount;
-
-		// 다음 프레임(Tick)을 위해 PrevTime 을 현재 시간으로 셋팅
-		mPrevTime = mCurrTime;
-
-		// 음수가 되지 않게 한다.
-		// 프로세서가 절전 므로 들어가거나 실행이 다른 프로세서랑 엉키면
-		// 간혹 음수가 된다고함
-		if (mDeltaTime < 0.0)
-		{
-			mDeltaTime = 0.0;
-		}
-	}
-
-private:
-	double mSecondsPerCount;
-	double mDeltaTime;
-
-	__int64 mBaseTime; // 총 시간
-	__int64 mPausedTime; // 일시정지 된 만큼의 시간
-	__int64 mStopTime; // 정지된 시점의 시간
-	__int64 mPrevTime; 
-	__int64 mCurrTime; 
-
-	bool mStopped;
-};
-
-class DxException
-{
-public:
-	DxException() = default;
-	DxException(HRESULT hr, const std::wstring& functionName, const std::wstring& filename, int lineNumber) :
-		ErrorCode(hr),
-		FunctionName(functionName),
-		Filename(filename),
-		LineNumber(lineNumber)
-	{
-	}
-
-	std::wstring ToString()const
-	{
-		// Get the string description of the error code.
-		_com_error err(ErrorCode);
-		std::wstring msg = err.ErrorMessage();
-
-		return FunctionName + L" failed in " + Filename + L"; line " + std::to_wstring(LineNumber) + L"; error: " + msg;
-	}
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
 
 
-	HRESULT ErrorCode = S_OK;
-	std::wstring FunctionName;
-	std::wstring Filename;
-	int LineNumber = -1;
-};
 
-inline std::wstring AnsiToWString(const std::string& str)
-{
-	WCHAR buffer[512];
-	MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, buffer, 512);
-	return std::wstring(buffer);
-}
+// HWND - Handle WiNDow
+// 주 창의 핸들, 창 핸들은 생성된 창을 식별하는 용도로 쓰임
+HWND ghMainWnd = 0; // gh - Get Handle
 
-inline void ThrowIfFailed(HRESULT hr)
-{
-	// DirectX API 오류를 탐지하기 위해 이 줄에 중단점 설정
+// Windows 응용 프로그램의 초기화에 필요한 코드를 담은 함수
+// 초기화에 성공하면 true, 아니면 false
+// HINSTANCE  - Hangle + Instance
+bool InitWindowsApp(HINSTANCE instanceHandle, int show);
 
-	std::wstring wfn = AnsiToWString(__FILE__);
-	if (FAILED(hr))
-		throw DxException(hr, wfn, wfn, __LINE__);
-	
-}
+// 메시지 루프 코드를 담은 함수
+int Run();
 
-#pragma region vals
-int mClientWidth = 800;
-int mClientHeight = 800;
-int mCurrBackBuffer = 0;
 
-bool mAppPaused = false;
-bool m4xMsaaState = false;
-unsigned int m4xMsaaQuality = 0;
-const int SwapChainBufferCount = 2;
+// 주 창이 받은 사건들을 처리하는 창 프로시저 함수
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM IParam);
 
-GameTimer mTimer;
-
-std::wstring mMainWndCaption = L"d3d App";
-HWND      mhMainWnd = nullptr;
-#pragma endregion
-
-void CalculateFrameStats()
-{
-	// Code computes the average frames per second, and also the 
-	// average time it takes to render one frame.  These stats 
-	// are appended to the window caption bar.
-
-	static int frameCnt = 0;
-	static float timeElapsed = 0.0f;
-
-	frameCnt++;
-
-	// Compute averages over one second period.
-	if ((mTimer.TotalTime() - timeElapsed) >= 1.0f)
-	{
-		float fps = (float)frameCnt; // fps = frameCnt / 1
-		float mspf = 1000.0f / fps;
-
-		std::wstring fpsStr = std::to_wstring(fps);
-		std::wstring mspfStr = std::to_wstring(mspf);
-
-		std::wstring windowText = mMainWndCaption +
-			L"    fps: " + fpsStr +
-			L"   mspf: " + mspfStr;
-
-		SetWindowText(mhMainWnd, windowText.c_str());
-
-		// Reset for next average.
-		frameCnt = 0;
-		timeElapsed += 1.0f;
-	}
-}
-
-void Update(const GameTimer& gt)
-{
-
-}
-
-void Draw(const GameTimer& gt)
-{
-
-}
-
-int Run()
-{
-	// MSG는 스레드 메시지 큐의 메시지 정보를 포함함
-	MSG msg = { 0 };
-
-	mTimer.Reset();
-
-	// msg.message는 메시지 식별자 ( 종료 메시지가 아니면 반복 )
-	while (msg.message != WM_QUIT)
-	{
-		// Windows 메시지가 있으면 처리함
-		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-		// 없으면 애니메이션 / 게임 작업을 수행함
-		else
-		{
-			mTimer.Tick();
-
-			if (!mAppPaused)
-			{
-				CalculateFrameStats();
-				Update(mTimer);
-				Draw(mTimer);
-			}
-			else
-			{
-				Sleep(100);
-			}
-		}
-	}
-	return (int)msg.wParam;
-}
-
-#pragma endregion
+// windows 응용 프로그램의 주 진입점. 콘솔 프로그램의 main()에 해당
 
 
 
 
-#pragma endregion
